b5d72071b4e5a14950465c8d5512b04a44510bd3
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-12 08:51:01 +0530

    implemented the Usecase 2

--- SnakeLadderSimulator/Program.cs
+++ SnakeLadderSimulator/Program.cs
@@ -11,8 +11,16 @@ namespace SnakeLadderSimulator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake Ladder Single Player Game");
+            Random random = new Random();
             int playerPosition = 0;
+
             Console.WriteLine("initialized the player position to {0}", playerPosition);
+            Console.WriteLine("Rolling the dice");
+
+            int diceRoll = random.Next(1, 7);
+            Console.WriteLine("\nDice Number: {0}",diceRoll);
+            playerPosition+=diceRoll;
+            Console.WriteLine("\nPlayer position: {0}",playerPosition);
             Console.ReadLine();
         }
     }
