6253ad083d3c2cf1eeabc7435a24ec0b80824ee5
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-12 09:33:12 +0530

    implemented the Use Case 4

--- SnakeLadderSimulator/Program.cs
+++ SnakeLadderSimulator/Program.cs
@@ -17,30 +17,33 @@ namespace SnakeLadderSimulator
             Console.WriteLine("initialized the player position to {0}", playerPosition);
             Console.WriteLine("Rolling the dice");
 
-            int diceRoll = random.Next(1, 7);
-            Console.WriteLine("\nDice Number: {0}\n",diceRoll);
-            
+            while (playerPosition <= 100)
+            {
+
+                int diceRoll = random.Next(1, 7);
+                Console.WriteLine("\nDice Number: {0}\n", diceRoll);
+
 
                 int option = random.Next(1, 4);
                 switch (option)
                 {
                     case 1:
-                            Console.WriteLine("No play\nplayer position: {0}", playerPosition);
-                            break;
-                  
-                    case 2:
-                            playerPosition += diceRoll;
-                            Console.WriteLine("Ladder\nplayer position : {0}",playerPosition);
-                            break ;
+                        Console.WriteLine("No play\nplayer position: {0}", playerPosition);
+                        break;
 
-                   case 3:   
-                            playerPosition -= diceRoll;
-                            if(playerPosition < 0)
+                    case 2:
+                        playerPosition += diceRoll;
+                        Console.WriteLine("Ladder\nplayer position : {0}", playerPosition);
+                        break;
+
+                    case 3:
+                        playerPosition -= diceRoll;
+                        if (playerPosition < 0)
                             playerPosition = 0;
-                            Console.WriteLine("Snake\nplayer position : {0}", playerPosition);
-                            break;
+                        Console.WriteLine("Snake\nplayer position : {0}", playerPosition);
+                        break;
                 }
-              
+            }
             Console.ReadLine();
         }
     }
