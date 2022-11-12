5c54e0d60595c9c355a72621c0a4bd195ebd3097
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-12 08:20:54 +0530

    Added the Welcome Message

---
+++ SnakeLadderSimulator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake Ladder Single Player Game");
+            Console.ReadLine();
+        }
+    }
+}
